bf3a2e106e2300df79601047b10aa47d910c82af
Shivangi Indurakhya <61985839+shivangi1102@users.noreply.github.com>
2021-09-01 09:32:21 +0530

    Update Searchnews.js

--- src/coponents/Searchnews.js
+++ src/coponents/Searchnews.js
@@ -6,17 +6,10 @@ import { useDispatch } from 'react-redux'
 import { searchNews } from '../redux/actions/productaction'
 import News from './News'
 
-
-
 const Searchnews = () => {
     const {search}=useParams()
     const dispatch=useDispatch()
-    
-
-    
     var url=`https://newsapi.org/v2/everything?apiKey=79f1ebf121b14cca9767657adca2b65f&q=${search}`
-    
-   
    console.log(search)
     const fetchSearch= useCallback(async ()=>{
         
@@ -27,9 +20,7 @@ const Searchnews = () => {
         
         dispatch(searchNews(data));
         
-        
         }
-        
         catch(error){
             console.log(error)
         }
